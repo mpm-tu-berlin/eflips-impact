654db9f716f375d6fdedc964c6baa3ca512918eb
Shuyao Guo <shuyao.guo@tu-berlin.de>
2026-07-06 12:06:30 +0200

    bump version to 2.1.0

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = "eflips-impact"
 copyright = "2026, MPM TU Berlin"
 author = "MPM TU Berlin"
-release = "2.0.0"
+release = "2.1.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
